ce6c2e530ff826ae5ca80cca93a6bd8d5ddf61de
name <e@ma.il>
2018-08-20 23:55:15 +0800

    Remove unnecessary code and macros from build.rs and cmark_export.h; Edit .gitignore to ignore test directory

--- build.rs
+++ build.rs
@@ -26,16 +26,13 @@ fn main() {
         "houdini_html_e.c",
         "houdini_html_u.c",
         "cmark_ctype.c",
-        //  we don't need main, otherwise we are defining main twice
-        //  (the other one is the writer2 main)
-        //  "main.c",
+
+        //  there is a main.c but we don't need it, otherwise we are defining
+        //  main twice (the other one is the writer2 main, our own main)
     ];
 
     cc::Build::new()
         .include("src/cmark/csrc")
-        //  don't even build a DLL, be static
-        //  .define("libcmark_EXPORTS", None)
-        .define("CMARK_STATIC_DEFINE", None)
         .files(files.into_iter().map(|file| {
             dir.join(PathBuf::from(file))
         }))
@@ -53,59 +50,3 @@ fn main() {
         .expect("could not write bindings");
 }
 
-/*
-fn main() {
-    let dir = PathBuf::from("src/cmark/csrc");
-    let files = vec![
-        "cmark.c",
-        "node.c",
-        "iterator.c",
-        "blocks.c",
-        "inlines.c",
-        "scanners.c",
-        "utf8.c",
-        "buffer.c",
-        "references.c",
-        "render.c",
-        "man.c",
-        "xml.c",
-        "html.c",
-        "commonmark.c",
-        "latex.c",
-        "houdini_href_e.c",
-        "houdini_html_e.c",
-        "houdini_html_u.c",
-        "cmark_ctype.c",
-        //  we don't need main, otherwise we are defining main twice
-        //  (the other one is the writer2 main)
-        //  "main.c",
-    ];
-
-    /*
-     *  we need to #define libcmark_EXPORTS while building cmark
-     *  because cmark_config.h #define's the export header CMARK_EXPORT as
-     *  __declspec(dllexport) properly only if libcmark_EXPORTS is defined.
-     *
-     *  the cmark source code we are using is originally intended to be
-     *  built using cmake, which defines this symbol as per CMakeLists.txt.
-     */
-
-    cc::Build::new()
-        .include("src/cmark/csrc")
-        .define("libcmark_EXPORTS", None)
-        .files(files.into_iter().map(|file| {
-            dir.join(PathBuf::from(file))
-        }))
-        .compile("cmark");
-
-    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
-    println!("cargo:rustc-link-search=native={}", out_dir.display());
-
-    let bindings = bindgen::Builder::default()
-        .header("src/cmark/csrc/wrapper.h")
-        .generate()
-        .expect("could not generate bindings for cmark");
-
-    bindings.write_to_file(out_dir.join("bindings.rs"))
-        .expect("could not write bindings");
-}*/
--- src/cmark/csrc/cmark_export.h
+++ src/cmark/csrc/cmark_export.h
@@ -2,27 +2,6 @@
 #ifndef CMARK_EXPORT_H
 #define CMARK_EXPORT_H
 
-/*
-#ifdef CMARK_STATIC_DEFINE
-#  define CMARK_EXPORT
-#  define CMARK_NO_EXPORT
-#else
-#  ifndef CMARK_EXPORT
-#    ifdef libcmark_EXPORTS
-         We are building this library 
-#      define CMARK_EXPORT __declspec(dllexport)
-#    else
-         We are using this library 
-#      define CMARK_EXPORT __declspec(dllimport)
-#    endif
-#  endif
-
-#  ifndef CMARK_NO_EXPORT
-#    define CMARK_NO_EXPORT 
-#  endif
-#endif
-*/
-
 #ifndef CMARK_EXPORT
 #define CMARK_EXPORT
 #endif
